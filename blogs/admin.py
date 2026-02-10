5f707fc8d96c4375e0c67807a0a3631effdab882
RichardWachara <richardwachara99@gmail.com>
2026-02-10 15:42:38 +0300

    Added the blog model and edited the Admin view to view more details on the admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Blog
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    # We tell django which field will be pre-populated and what fields will be populated from
+    # i.e prepopulate the slug field using the title field. The title field is a tuple because the slug can be created from many fields
+    prepopulated_fields = {'slug' : ('title',)}
+    # To edit the display on the jango admin platfrom we can use the list_display 
+    list_display = ("title", "category","author","status","is_featured")
+    search_fields = ("id","title","status","category__category_name")
+    # A tuple must have a comma at the end if there is only one item.
+    list_editable = ("is_featured",)
+
+
 admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -12,3 +13,29 @@ class Category(models.Model):
     # self refers to the current instance of the class.
     def __str__(self):
         return self.category_name
+
+# Choices to make a dropdown
+
+STATUS_CHOICES = (
+    ("draft", "Draft"),
+    ("published","Published")
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    #URL-safe label used to uniquely identify a blog. Normally a heading separated with hyphens.
+    slug = models.SlugField(max_length=150,unique=True,blank=True)
+    # Each blog has to be associated with a one category. Once a category is deleted we need to delete all the blogs in that category(CASCADE)
+    # This is a one to many relationship.
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to="uploads/%Y/%m/%d")
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=10000)
+    status = models.TextField(choices=STATUS_CHOICES,default='draft')
+    is_featured =  models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True) # Auto add now is the first time the object is created
+    updated_at = models.DateTimeField(auto_now=True) # Auto add is updated every time the models is saved
+
+    def __str__(self):
+        return self.title
